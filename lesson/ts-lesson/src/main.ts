545c3c8fe1b15870dfe149a60f5115d7b3348a6b
Mirodil <odilfayz22@gmail.com>
2023-05-28 22:33:42 +0500

    .

--- lesson/ts-lesson/src/main.ts
+++ lesson/ts-lesson/src/main.ts
@@ -1,3 +1,42 @@
-const names: number = 27;
+// let myName: string | null;
+//
+// myName = "Mirodil";
+//
+// console.log(myName);
+//
+// myName = null;
+//
+// console.log(myName);
 
-console.log(names);
+// let items: string[] | null = ["Mirodil", "Qwerty"];
+// let items2 = ["Mirodil", 3];
+
+// type Account = {
+//   name: string;
+//   balance: number;
+// };
+
+interface Account {
+  name: string;
+  balance: number;
+  deposit: () => void;
+}
+
+// const account2: Account = {
+//   name: "John",
+//   balance: 10,
+// };
+//
+// const account: Account = {
+//   name: "John",
+//   balance: 10,
+//   account: account2,
+// };
+//
+// console.log("account:", account);
+
+class Human implements Account {
+  constructor(public name: string, public balance: number) {}
+
+  deposit(): void {}
+}
